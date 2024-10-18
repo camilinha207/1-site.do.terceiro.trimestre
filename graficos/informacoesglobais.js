b3b5fc3337427e083e67df71da9f9f961e6a8e67
camilinha207 <c.avelino@escola.pr.gov.br>
2024-10-18 07:50:24 -0300

    aie

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -1,5 +1,6 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
+console.log(url)
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
