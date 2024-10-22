502e86c638f87fd50a1fbb5c2b22f33e91172822
camilinha207 <c.avelino@escola.pr.gov.br>
2024-10-22 12:13:42 -0300

    rgtth

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -4,7 +4,7 @@ console.log(url)
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 5040000000)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
