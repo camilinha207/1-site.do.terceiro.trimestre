c8f0d63371d50f0fb3d9cbd6675c33b40e2f27e1
camilinha207 <c.avelino@escola.pr.gov.br>
2024-10-22 12:25:04 -0300

    frg

--- graficos/informacoesglobais.js
+++ graficos/informacoesglobais.js
@@ -5,7 +5,7 @@ async function vizualizarInformacoesGlobais() {
     const dados = await res.json()
     console. log(dados);
     const paragrafo = document.createElement('p')
-    
+    paragrafo.classList.add('graficos-container__texto')
 }
 
 vizualizarInformacoesGlobais()
